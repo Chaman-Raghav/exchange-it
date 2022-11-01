b2623c04076096e26ae35ef06487cfcce1551646
Aadarash_Raj <aadarash.raj@vicesoftware.com>
2022-11-01 12:59:50 +0530

    implemented code to perform crud operation

--- Api/Controllers/UsersController.cs
+++ Api/Controllers/UsersController.cs
@@ -7,29 +7,48 @@ using Api.Domain.Users;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.Extensions.Configuration;
 using System.Net;
+using Api.Domain;
+using System.Linq;
 
 namespace Api.Controllers
 {
-
-
     // TODO: Need to update working of Users Controller.
     [Route("api/[controller]")]
     [ApiController]
-    [Authorize]
+   // [Authorize]
     public class UsersController : ControllerBase
     {
         private readonly IMediator _mediator;
         private IConfiguration Configuration { get; }
         private readonly UserService _userService;
         private object UserId;
+        private readonly ViceContext _context;
 
-        public UsersController(IMediator mediator, IConfiguration configuration, UserService userService)
+        public UsersController(IMediator mediator, IConfiguration configuration, UserService userService, ViceContext context)
         {
             _mediator = mediator;
             Configuration = configuration;
             _userService = userService;
+            _context = context;
         }
 
+        
+        [HttpPost]
+        [Route("create")]
+        public async Task<ActionResult> CreateUsers(User model)
+        {
+            try
+            {
+                _context.Users.Add(model);
+                _context.SaveChanges();
+
+            }
+            catch (UnauthorizedAccessException err)
+            {
+                return StatusCode((int)HttpStatusCode.Forbidden, err.Message);
+            }
+            return Ok("Data Inserted");
+        }
 
         // GET: api/users
         [HttpGet]
@@ -37,9 +56,10 @@ namespace Api.Controllers
         {
             try
             {
-                var result = await _mediator.Send(new GetUsersQuery { });
+                // Return the list of data from the database
+                var data = _context.Users.ToList();
+                return Ok(data);
 
-                return new OkObjectResult(result);
             }
             catch (UnauthorizedAccessException err)
             {
@@ -53,8 +73,8 @@ namespace Api.Controllers
         {
             try
             {
-                var users = await _mediator.Send(new GetUsersQuery { Id = id, UserId = UserId });
-                return (ActionResult<User>)users;
+                var users =  _context.Users.Where(x => x.Id == id).ToList();
+                return Ok(users);
             }
             catch (UnauthorizedAccessException err)
             {
@@ -69,11 +89,38 @@ namespace Api.Controllers
         {
             try
             {
-                await _mediator.Send(new DeleteUserCommand { Ids = new[] { id }, UserId = UserId });
-
-                return id;
+                var data =  _context.Users.FirstOrDefault(x => x.Id == id);
+                if (data != null)
+                {
+                    _context.Users.Remove(data);
+                    _context.SaveChanges();
+                   
+                }
+                return Ok("user got deleted");
             }
             catch (UnauthorizedAccessException err)
+            {
+                return StatusCode((int)HttpStatusCode.Forbidden, err.Message);
+            }
+        }
+
+        [HttpPost]
+        public ActionResult Update( User model)
+        {
+            try
+            {
+                var data = _context.Users.FirstOrDefault(x => x.Id == model.Id);
+
+                // Checking if any such record exist
+                if (data != null)
+                {
+                    data.Id = model.Id;
+                    data.Name = model.Name;
+                    _context.SaveChanges();
+                }
+                return Ok("User data Updated");
+            }
+            catch(UnauthorizedAccessException err)
             {
                 return StatusCode((int)HttpStatusCode.Forbidden, err.Message);
             }
